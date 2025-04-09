3fc87374b7bef9ccf19fe14684b64fa9abc50b80
xuanlongbui <xuanlong1372000@gmail.com>
2025-04-09 16:24:20 +0700

    github action, auto build

---
+++ test/buid-scripts/glcts.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+echo "$DIR"
+pushd $DIR/../opengles-cts
+git checkout remotes/origin/opengl-es-cts-3.2.11
+patch -p1 < $DIR/patch/patch.diff
+python3 external/fetch_sources.py --insecure
+mkdir build; cd build
+cmake .. -DDEQP_TARGET=wayland -DGLCTS_GTF_TARGET=gles3 -DCMAKE_BUILD_TYPE=Release
+cmake --build external/openglcts/
+popd
+exit 0
---
+++ test/buid-scripts/rvgpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+pushd $DIR/../..
+# cd remote-virtio-gpu
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+make -C build
+popd
+exit 0
